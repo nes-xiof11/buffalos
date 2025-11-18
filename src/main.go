d8b1a24c24e06e88c9ca888a4da9ae4a1a07086a
Eloide Novela <eloide.novela@outlook.com>
2025-11-18 16:42:05 +0200

    stable version

--- src/main.go
+++ src/main.go
@@ -8,16 +8,12 @@ import (
 func main() {
 	args := os.Args
 	if len(args) < 2 {
-		if args[1] == "--rename" || args[1] == "-rn" {
-			configurations.Rename(args)
-		} else if args[1] == "--help" || args[1] == "-h" {
+		if args[1] == "--help" || args[1] == "-h" {
 
 		} else if args[1] == "--version" || args[1] == "-v" {
 
-		} else if args[1] == "--install" || args[1] == "-i" {
-
-		} else if args[1] == "--update" || args[1] == "-u" {
-
+		} else if args[1] == "--run" || args[1] == "-r" {
+			configurations.Run()
 		}
 	}
 }
