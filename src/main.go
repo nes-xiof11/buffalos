a77fd844f6415dcb34671dd3cc35b71866f63f04
Eloid-novela <111151103+nes-xiof11@users.noreply.github.com>
2025-11-18 14:17:04 +0200

    Remove rename and update command options
    Removed the rename and update command options from main function.

--- src/main.go
+++ src/main.go
@@ -1,22 +1,17 @@
 package main
 
 import (
-	"buffalos/src/internal/configurations"
 	"os"
 )
 
 func main() {
 	args := os.Args
 	if len(args) < 2 {
-		if args[1] == "--rename" || args[1] == "-rn" {
-			configurations.Rename(args)
-		} else if args[1] == "--help" || args[1] == "-h" {
+		if args[1] == "--help" || args[1] == "-h" {
 
 		} else if args[1] == "--version" || args[1] == "-v" {
 
-		} else if args[1] == "--install" || args[1] == "-i" {
-
-		} else if args[1] == "--update" || args[1] == "-u" {
+		} else if args[1] == "--run" || args[1] == "-r" {
 
 		}
 	}
